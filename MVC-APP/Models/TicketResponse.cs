37023d961ee033dd5dca5dec6ca93339f0948c6b
YusraAdnan <101713869+YusraAdnan@users.noreply.github.com>
2023-08-20 16:01:06 +0000

    Database (#10)
    authored-by: Yusra Adnan <yusraadnan@gmail.com>

---
+++ MVC-APP/Models/TicketResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace MVCAPP.Models
+{
+    public class TicketResponse
+    {
+        [Key]
+        public int ResponseId {get; set;}
+        [Required]
+        [ForeignKey("TicketDetail")]
+        public string TicketId {get; set;}
+        [Required]
+        [ForeignKey("TeamDev")]
+        public string DevId {get; set;}
+        [Required]
+        [ForeignKey("UserLogin")]
+        public string Email {get; set;}
+        public string? ResponseMessage {get; set;}
+    }
+}
---
+++ MVC-APP/Models/UserInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+namespace MVCAPP.Models
+{
+    public class UserInfo{
+        [Key]
+        public int UserId {get; set;}
+        [Required]
+        [ForeignKey("UserLogin")]
+        public string Email {get; set;}
+        public string? Name {get; set;} 
+        public string? PhoneNumber {get; set;}
+    }
+}
