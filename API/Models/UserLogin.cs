f45eef1230674274186cff3f9ac4fab286d511a8
Yusra Adnan <yusraadnan@gmail.com>
2023-08-22 20:03:21 +0200

    API Database

---
+++ API/Models/UserLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace api.Models;
+
+public partial class UserLogin
+{
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,13 @@
+using api.Models;
+using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddDbContext<StudentSupportXbcadContext>(
+    options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); }
+    );
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
