55bf17957b34179443fb707d327ce77a226f3565
Pranhav Maistry <74468682+HumanClone@users.noreply.github.com>
2023-09-05 14:12:15 +0200

    review post ticket controller

--- MVC-APP/Controllers/TicketController.cs
+++ MVC-APP/Controllers/TicketController.cs
@@ -2,9 +2,10 @@
 using System.Net;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Net.Http.Json;
 using MVCAPP.Data;
 using MVCAPP.Models;
-
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 namespace mvc_app.Controllers;
@@ -12,75 +13,95 @@ namespace mvc_app.Controllers;
 public class TicketController : Controller
 {
     private readonly ApplicationDbContext _context;
+    private static HttpClient sharedClient = new()
+    {
+        BaseAddress = new Uri("https://supportsystemapi.azurewebsites.net/api/"),
+    };
 
     public TicketController(ApplicationDbContext context)
     {
-       this._context = context;
+        this._context = context;
     }
 
     [HttpGet]
-        public IActionResult Create(string categoryName)
-        {
-           
-            ViewBag.SelectedCategoryName = categoryName;
-            return View();
-        }  
+    public IActionResult Create(string categoryName)
+    {
+        ViewBag.SelectedCategoryName = categoryName;
+        return View();
+    }  
     
-     [HttpPost]
-     public IActionResult Create(TicketDetail ticketDetail, string categoryName) 
+    [HttpPost]
+    public async Task<IActionResult> Create(TicketDetail ticketDetail, string categoryName) 
+    {
+        try
         {
-            try
+            //finds the selected category's id 
+            int categoryId = _context.Categories
+            .Where(c => c.CategoryName == categoryName)
+            .Select(c=> c.CategoryId)
+            .FirstOrDefault();
+
+            if (ModelState.IsValid)
             {
-             //finds the selected category's id 
-               int categoryId = _context.Categories
-               .Where(c => c.CategoryName == categoryName)
-               .Select(c=> c.CategoryId)
-               .FirstOrDefault();
-
-                if (ModelState.IsValid)
+                var Ticket = new TicketDetail()
                 {
-                    var Ticket = new TicketDetail()
+                    
+                    TicketId = ticketDetail.TicketId,
+                    CategoryId = categoryId.ToString(),
+                    UserId = ticketDetail.UserId,
+                    DevId = ticketDetail.DevId,
+                    DateIssued = DateTime.Now,
+                    MessageContent = ticketDetail.MessageContent,
+                    Status = ticketDetail.Status,
+                    CategoryName = categoryName,
+                    
+                };//send this object to api
+
+                try
+                {
+                    HttpResponseMessage response = await sharedClient.PostAsJsonAsync("ticket/createuserticket",Ticket);
+
+                    if (response.IsSuccessStatusCode)
                     {
-                        
-                        TicketId = ticketDetail.TicketId,
-                        CategoryId = categoryId.ToString(),
-                        UserId = ticketDetail.UserId,
-                        DevId = ticketDetail.DevId,
-                        DateIssued = ticketDetail.DateIssued,
-                        MessageContent = ticketDetail.MessageContent,
-                        Status = ticketDetail.Status,
-                        CategoryName = categoryName,
-                       
-                        
-                    };//send this object to api
-                        
-                
-                    _context.TicketDetails.Add(Ticket);
-                    _context.SaveChanges();
-                  
-                    Console.WriteLine("Added to database");
-                    return RedirectToAction("ViewTicket");
+                        Console.WriteLine("POST request successful");
+                        Console.WriteLine("Added to database");
+                        return RedirectToAction("ViewTicket");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Request failed with status code: {response.StatusCode}");
+                        return View();
+                    }
                 }
-                else
+                catch (HttpRequestException ex)
                 {
-                    foreach (var modelState in ModelState.Values)
-                        {
-                            foreach (var error in modelState.Errors)
-                            {
-                                Console.WriteLine($"Model Error: {error.ErrorMessage}");
-                            }
-                        }
-
+                    Console.WriteLine($"Request error: {ex.Message}");
                     return View();
                 }
+                    
+                
+                
             }
-            catch (Exception ex)
+            else
             {
-                TempData["errorMessage"] = ex.Message;
-                return View();
+                foreach (var modelState in ModelState.Values)
+                    {
+                        foreach (var error in modelState.Errors)
+                        {
+                            Console.WriteLine($"Model Error: {error.ErrorMessage}");
+                        }
+                    }
 
+                return View();
             }
         }
+        catch (Exception ex)
+        {
+            TempData["errorMessage"] = ex.Message;
+            return View();
+
+        }
+    }
 
 
         [HttpGet]
@@ -99,7 +120,6 @@ public class TicketController : Controller
                     DateIssued = ticket.DateIssued,
                     Status = ticket.Status
                 };
-              
                 ticketList.Add(ticketDetail);
             }
 
