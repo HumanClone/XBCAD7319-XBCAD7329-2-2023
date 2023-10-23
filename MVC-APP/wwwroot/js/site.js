ddf605ab0af5a5dd0a9121cad59b1b7b80acb602
TrinityJayd <trinitytiratram@gmail.com>
2023-10-23 11:42:16 +0000

    Added pagination

--- MVC-APP/wwwroot/js/site.js
+++ MVC-APP/wwwroot/js/site.js
@@ -1,4 +1,45 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// Pagination
+  // Show 5 cards per page
+  var cardsPerPage = 5;
 
-// Write your JavaScript code.
+  // Get the card elements
+  var cards = document.querySelectorAll('.card.ticket-card');
+
+  // Calculate the total number of pages
+  var totalPages = Math.ceil(cards.length / cardsPerPage);
+
+  // Create a function to display the cards for a given page
+  function showCards(page) {
+    // Calculate the start and end indices for the cards to display
+    var startIndex = (page - 1) * cardsPerPage;
+    var endIndex = startIndex + cardsPerPage;
+
+    // Hide all cards
+    cards.forEach(function(card) {
+      card.style.display = 'none';
+    });
+
+    // Display the cards for the current page
+    for (var i = startIndex; i < endIndex; i++) {
+      if (cards[i]) {
+        cards[i].style.display = 'block';
+      }
+    }
+  }
+
+  // Create the pagination buttons
+  var paginationContainer = document.getElementById('pagination-container');
+  for (var i = 1; i <= totalPages; i++) {
+    var button = document.createElement('button');
+    button.classList.add('page-item','page-link');
+    button.textContent = i;
+    button.addEventListener('click', function() {
+      // Get the page number from the button text
+      var page = parseInt(this.textContent);
+      showCards(page);
+    });
+    paginationContainer.appendChild(button);
+  }
+
+  // Show the cards for the first page initially
+  showCards(1);
